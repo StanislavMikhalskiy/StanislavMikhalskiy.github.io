35f368d4213d643fafdee17e0ad07fcac2438d1a
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2019-03-20 13:49:28 +0300

    Update action_jira_config.js

--- action_jira_config.js
+++ action_jira_config.js
@@ -1,5 +1,11 @@
 var action_jira_config_v = '1.0';
 
-function GetTestAlert(value){
-  alert(value+' Go-go!');
-}
+// Настрокий Jira
+var jiraURL = "https://jira.action-media.ru";
+var jCreateTaskURL="/rest/api/2/issue/";
+var jGetIssue="/rest/api/2/issue/";
+var jIssueLinkURL = "/rest/api/2/issueLink/";
+var jFieldNameEpicName="customfield_10102";
+var jFieldNameEpicLink="customfield_10100";
+var jFindURL="/rest/api/2/search";
+var jGetSprint="/rest/agile/1.0/board/";
