20705072b7a76406c5d9721540b4cf9a17b31192
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2019-03-20 13:41:00 +0300

    Update action_jira_getdata.js

--- action_jira_getdata.js
+++ action_jira_getdata.js
@@ -1,9 +1,5 @@
 var action_jira_getdata_v = '1.0';
 
-// ==UserScript==
-// @require      https://stanislavmikhalskiy.github.io/action_jira_config.js
-// ==/UserScript==
-
 function test(){
    GetTestAlert('action_jira_config_v = '+action_jira_config_v);
 }
