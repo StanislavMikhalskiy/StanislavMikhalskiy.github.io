e153e01e918cd36ba6d408cd4492883b6fc3ee1a
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2019-03-20 16:13:53 +0300

    Update action_jira_workload.js

--- action_jira_workload.js
+++ action_jira_workload.js
@@ -10,15 +10,18 @@ function GetFutureSprintSmart(flevel){
         if (functionResponse.state) {
             futureSprint = functionResponse.value;
             console.log(f+'Спринт определен успешно: '+futureSprint);
+            console.log(f+'GetFutureSprintSmart - завершение работы функции');
             return true;
         } else {
             errorMessage += "<p>Спринт не был определен</p>";
             errorMessage += "<p>"+functionResponse.errorMsg+"</p>";
             console.log(f+'Информация об ошибке: '+functionResponse.errorMsg);
+            console.log(f+'GetFutureSprintSmart - завершение работы функции');
             return false;
         }
     } else {
         console.log(f+'Спринт уже был определен: '+futureSprint);
+        console.log(f+'GetFutureSprintSmart - завершение работы функции');
         return true;
     }
     
