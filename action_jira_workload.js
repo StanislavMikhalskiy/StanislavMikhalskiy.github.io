ca277541272ac6421443b7e18afe3753f78fd220
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2019-03-20 15:56:03 +0300

    Create action_jira_workload.js

---
+++ action_jira_workload.js
@@ -0,0 +1,25 @@
+var action_jira_workload_v = '1.0';
+
+function GetFutureSprintSmart(flevel){
+    var f = flevel+'- ';
+    console.log(f+'GetFutureSprintSmart - Запуск функции');
+    // если мы уже запрашивали спринт в этой сессии и у нас есть его значение, то повторно запрашивать не будем
+    if (futureSprint.length == 0) {
+        console.log(f+'Спринт еще не был определен');
+        functionResponse = JiraGetFutureSprint(boardId,f);
+        if (functionResponse.state) {
+            futureSprint = functionResponse.value;
+            console.log(f+'Спринт определен успешно: '+futureSprint);
+            return true;
+        } else {
+            errorMessage += "<p>Спринт не был определен</p>";
+            errorMessage += "<p>"+functionResponse.errorMsg+"</p>";
+            console.log(f+'Информация об ошибке: '+functionResponse.errorMsg);
+            return false;
+        }
+    } else {
+        console.log(f+'Спринт уже был определен: '+futureSprint);
+        return true;
+    }
+    return false;
+}
