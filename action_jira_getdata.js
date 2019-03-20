3b54f125d6700fdc6cb1385b946c5f2d483fc814
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2019-03-20 16:25:16 +0300

    Update action_jira_getdata.js

--- action_jira_getdata.js
+++ action_jira_getdata.js
@@ -34,3 +34,37 @@ function JiraGetFutureSprint(value,flevel){
     
     return localFunctionResponse;
 }
+
+function GetTimeToWork(value,flevel){
+    var f = flevel+'- ';
+    console.log(f+'GetTimeToWork - Запуск функции');
+    var localFunctionResponseArray = {value:[],state:false,errorMsg:""},
+        sprint = value,
+        URL = "https://script.google.com/macros/s/AKfycbwJ8QiZKAyFVpjFbF037JQ6UmDaaq_SGPlu1eANAK9eHpEMjV0/exec?sprint="+sprint;
+    var jqxhr = $.ajax({
+    url: URL,
+    type: "GET",
+    success: function(jqdata) {
+        console.log(f+'Запрос успешно отработал');
+        console.log('jqdata.length = '+jqdata.length);
+        if (jqdata.length > 0) {
+            localFunctionResponseArray.state = true;
+            localFunctionResponseArray.value = jqdata;
+        } else {
+            localFunctionResponseArray.state = false;
+            localFunctionResponseArray.errorMsg = "Данные по планируемой загрузке не найдены, проверьте имя спринта или наличие данных ("+URL+")";
+        }
+    },
+    async:false
+    })
+    //.done(function() { result="second success"; })
+    .fail(function(errmsg) {
+        console.log(f+'Ошибка обработки '+errmsg.responseText);
+        localFunctionResponseArray.errorMsg = "Ошибка получения данных по url "+URL;
+    })
+    //.always(function() { result="finished"; });
+    ;
+    
+    console.log(f+'GetTimeToWork - завершение работы функции');
+    return localFunctionResponseArray;
+}
