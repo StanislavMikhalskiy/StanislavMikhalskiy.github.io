2d62a995e799036e6c2233e064aed30b259620bf
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2019-03-20 16:16:29 +0300

    Update action_jira_workload.js

--- action_jira_workload.js
+++ action_jira_workload.js
@@ -28,3 +28,33 @@ function GetFutureSprintSmart(flevel){
     console.log(f+'GetFutureSprintSmart - завершение работы функции');
     return false;
 }
+
+function GetTimeToWorkSmart(flevel){
+    var f = flevel+'- ';
+    console.log(f+'GetTimeToWorkSmart - Запуск функции');
+    // получаем данные по ресурсам из гугла
+    // если мы уже запрашивали спринт в этой сессии и у нас есть его значение, то повторно запрашивать не будем
+    if (planTime.length<1) {
+        console.log(f+'Данные по доступному времени еще не запрашивались');
+        functionResponseArray = GetTimeToWork(futureSprint);
+        if (functionResponseArray.state) {
+            planTime = functionResponseArray.value;
+            console.log(f+'Данные по доступному времени получены успешно');
+            console.log(f+'GetTimeToWorkSmart - завершение работы функции');
+            return functionResponseArray.state;
+        } else {
+            errorMessage += "<p>Данные по доступному времени не получены</p>";
+            errorMessage += "<p>"+functionResponseArray.errorMsg+"</p>";
+            console.log(f+'Информация об ошибке: '+functionResponseArray.errorMsg);
+            console.log(f+'GetTimeToWorkSmart - завершение работы функции');
+            return functionResponseArray.state;
+        }
+    } else {
+        console.log(f+'Данные по доступному времени уже были получены');
+        console.log(f+'GetTimeToWorkSmart - завершение работы функции');
+        return true;
+    }
+    
+    console.log(f+'GetTimeToWorkSmart - завершение работы функции');
+    return false;
+}
