4989d37a84c92778adc8fc84144bbca4ac41d9b4
StanislavMikhalskiy <48533254+StanislavMikhalskiy@users.noreply.github.com>
2019-03-21 12:26:23 +0300

    Update action_jira_workload_common.js

--- action_jira_workload_common.js
+++ action_jira_workload_common.js
@@ -1,5 +1,5 @@
 var action_jira_workload_common_v = '1.0';
 
-var boardId = "136";
-var projectID = "SS";
-var futureSprint = "";
+var RoleCommon = [{RoleCode:'10206',RoleName:'Developers'},{RoleCode:'10404',RoleName:'QA'}];
+var planTime = [];
+var employees = [], issues = [];
